2906bf700f0cbf8cddfa827d214966470e033476
eduarf <fraude_f@yahoo.com>
2023-06-24 18:06:48 +0300

    header-styling and responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,19 @@ module.exports = {
       spacing: {
         '4vw': '4vw',
         '6vh': '6vh',
+        'calcSvg': 'calc(26vw + 16rem)',
+        '128': '30rem',
+        '156': '38rem',
+        '196': '45rem',
       },
       gridTemplateColumns: {
         "grid-column":
           "[container-start] 1fr [center-start] repeat(12, [col-start] minmax(min-content, 10rem) [col-end]) [center-end] 1fr [container-end]",
-      },
+          // '2-auto-fr': 'repeat(auto-fit, minmax(30rem, 1fr))',
+        },
       gridTemplateRows: {
         'grid-rows': '100vh repeat(11, min-content)',
+        
       },
       colors: {
         'primary': '#5a5af3',
