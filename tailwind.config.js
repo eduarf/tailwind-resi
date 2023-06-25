6d003ef26da29126d2158a15046a9da830d3a862
eduarf <fraude_f@yahoo.com>
2023-06-25 19:18:07 +0300

    porfolio grid

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,23 @@ module.exports = {
         },
       gridTemplateRows: {
         'grid-rows': '100vh repeat(11, min-content)',
-        
+        'portfolio': 'repeat(13, 8vh)', 
+      },
+      gridRowStart: {
+        '8': '8',
+        '9': '9',
+        '10': '10',
+        '11': '11',
+        '12': '12',
+        '13': '13',
+      },
+      gridRowEnd: {
+        '8': '8',
+        '9': '9',
+        '10': '10',
+        '11': '11',
+        '12': '12',
+        '13': '13',
       },
       colors: {
         'primary': '#5a5af3',
