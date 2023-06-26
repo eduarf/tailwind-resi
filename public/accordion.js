cedb45678dddef783ca99a5b80b7b6fa57beff14
eduarf <fraude_f@yahoo.com>
2023-06-26 17:39:59 +0300

    accordion-menu section is done

---
+++ public/accordion.js
@@ -0,0 +1,30 @@
+const accordionItems = document.querySelectorAll('#accordion-item');
+const chevron = document.querySelector('#chevron');
+accordionItems.forEach((item) => {
+    const accordionHeader = item.querySelector('#accordion-header');
+    const accordionContent = item.querySelector('#accordion-content');
+
+    accordionHeader.addEventListener('click', () => {
+        const isActive = accordionContent.classList.contains('active');
+        closeAllItems();
+
+        if(!isActive) {
+            accordionContent.classList.add('active');
+            accordionContent.style.maxHeight = '10rem';
+            chevron.setAttribute('class', 'fa-solid fa-chevron-up');
+            accordionHeader.classList.add('text-primary');
+        }
+    });
+});
+
+
+const closeAllItems = () => {
+    accordionItems.forEach((item) => {
+        const accordionContent = item.querySelector('#accordion-content');
+        const accordionHeader = item.querySelector('#accordion-header');
+        accordionContent.classList.remove('active');
+        accordionContent.style.maxHeight = '0';
+        chevron.setAttribute('class', 'fa-solid fa-chevron-down');
+        accordionHeader.classList.remove('text-primary');
+    });
+}; 
